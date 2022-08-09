01d69fbc2da3ea36d27e9a4ac067aa426abd1a09
Dominiqueiscoding <dmk9476@nyu.edu>
2022-08-09 13:57:14 -0400

    ninth commit

--- Quiz_class_file.py
+++ Quiz_class_file.py
@@ -1,22 +1,30 @@
 
 from questions import prompts
 class Quiz_game:
-    import json
 
     def __init__(self, asking):
         self.askings = asking
 
     def play_quiz(self, asking):
-        print("Please only input a letter \n")
         player_name = input("Please type your name: \n")
-        responses= {"Dominique" : ["B", "B", "B", "A"]}
+        responses= {"Dominique" : ["B", "B" , "B", "A"]}
         answer = []
-        for prompt, choices in asking:
-            answer.append(input(prompt, choices).upper())
+        print("Please enter the letter that cooresponds with your answer choice")
+        for prompt, choices in asking.items():
+            print(prompt)
+            answer.append(input(choices).upper())
             responses[player_name] = answer
+        def turning_json(self):
+            import json
+            with open('results.json', 'a') as results_json:
+                json.dump(responses, results_json)
+        turning_json(responses)
+
+                 
 
 
-Quiz_game(prompts)
+starting = Quiz_game(prompts)
+starting.play_quiz(prompts)
 
 
 
--- questions.py
+++ questions.py
@@ -1,6 +1,5 @@
-prompts = {"What food would you most like to eat?" : [ ("A", "popcorn") , ("B" "cookies"), ("C", "chips")],
-    "What is your favorite vacation spot?" : [ ("A", "beach") , ("B", "mountains"), ("C", "desert")],
-    "What color do you like the best?" : [ ("A", "blue"), ("B", "green"), ("C", "red")], 
-    "what would you most want to be?" : [("A", "loved"), ("B", "feared"), ("C", "respected")]}
+prompts = {"What food would you most like to eat?" : [ ("A", "popcorn ") , ("B", "cookies "), ("C", "chips ")],
+    "What is your favorite vacation spot?" : [ ("A", "beach ") , ("B", "mountains "), ("C", "desert")],
+    "What color do you like the best?" : [ ("A", "blue "), ("B", "green "), ("C", "red ")], 
+    "what would you most want to be?" : [("A", "loved "), ("B", "feared "), ("C", "respected ")]}
 
-poop = "poop"
